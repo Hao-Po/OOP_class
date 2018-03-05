55376f299803a5b660dfa122a3ec5b13a42d1a44
Y C Cheng <yccheng@csie.ntut.edu.tw>
2018-03-05 17:02:10 +0800

    moving animals in zoo

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
 
-int add(int a, int b) {
-  return a+b;
+double dotProduct(double a[], int dim_a, double b[], int dim_b) {
+  assert(dim_a == dim_b);
+  double answer = 0;
+  for (int i=0; i<dim_a; i++) {
+    answer += a[i]*b[i];
+  }
+  return answer;
 }
 
+void testDotProduct() {
+  double u[] = {1,1};
+  double v[] = {0,1};
+  double dp = dotProduct(u, 2, v, 2);
+  assert(1==dp);
+}
+
+void testDotProduct_2() {
+  double u[] = {1,1,0};
+  double v[] = {0,1,1};
+  double dp = dotProduct(u, 3, v, 3);
+  assert(1==dp);
+}
+
+void testDotProduct_3() {
+  double u[] = {1,0};
+  double v[] = {1,1,1};
+  double dp = dotProduct(u, 2, v, 3);
+}
 int main(int argc, char **argv) {
-  printf("Hello, C!\n");
-  int result = add(2,3);
-  assert(4==result);
+  printf("Hello, dot product!\n");
+
+  testDotProduct();
+  testDotProduct_2();
+  testDotProduct_3();
 }
