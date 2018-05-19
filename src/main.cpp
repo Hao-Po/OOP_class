2787335966a3f658187ac916e4893df39cc39469
Ice <wayispiderik@gmail.com>
2018-05-19 22:29:24 +0800

    hw8

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,19 @@
 using namespace std;
 
 int main(int argc,char* argv[]){
-  fstream file;
   for(int i=1;argv[i]!=0;i++){
-    file.open(argv[i],ios::in);
+    fstream file(argv[i]);
     int numberOfVertices;
-    string s[50];
-    Vector v[50];
     file >> numberOfVertices;
+    string s[numberOfVertices+1];
+    Vector v[numberOfVertices];
+    for(int i=0;i<numberOfVertices+1;i++){
+      string temp_s;
+      getline(file,temp_s);
+      s[i] = temp_s;
+    }
     for(int i=0;i<numberOfVertices;i++){
-      file >> s[i];
-      v[i] = *makeVectorFromString(s[i]);
+      v[i] = *makeVectorFromString(s[i+1]);
     }
     Polygon p = createPolygon(v,numberOfVertices);
     cout << "Length of polygon is: " << fixed << setprecision(2) << p.length() << endl
